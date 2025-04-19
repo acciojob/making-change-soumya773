fcfa945be7a56f702ad3ffa54bd85d0a2a48fd31
soumya773 <150830999+soumya773@users.noreply.github.com>
2025-04-19 21:08:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const makeChange = (c) => {
-  c = parseInt(c); // Ensure the input is treated as a number
+  c = parseInt(c); // convert to number
 
-  return {
-    q: Math.floor(c / 25),
-    d: Math.floor((c % 25) / 10),
-    n: Math.floor((c % 10) / 5),
-    p: c % 5
-  };
+  const q = Math.floor(c / 25);
+  c %= 25;
+
+  const d = Math.floor(c / 10);
+  c %= 10;
+
+  const n = Math.floor(c / 5);
+  c %= 5;
+
+  const p = c;
+
+  return { q, d, n, p };
 };
 
 // Do not change the code below
