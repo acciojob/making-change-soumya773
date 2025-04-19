3e63558cb80f83b04ecf77fdbddecce46ef471d9
soumya773 <150830999+soumya773@users.noreply.github.com>
2025-04-19 20:48:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const makeChange = (c) => {
-  // your name here
+  c = parseInt(c); // Ensure the input is treated as a number
+
+  return {
+    q: Math.floor(c / 25),
+    d: Math.floor((c % 25) / 10),
+    n: Math.floor((c % 10) / 5),
+    p: c % 5
+  };
 };
 
-// Do not the change the code below
+// Do not change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
